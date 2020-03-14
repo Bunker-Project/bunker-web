093f251c059b9f4d81e6b4c6d976e7ac45738abd
Douglas Breda <bredadouglas@gmail.com>
2020-03-14 01:17:29 -0300

    Added interface to add items

--- bunkerweb/src/App.js
+++ bunkerweb/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/Navbar';
 import Main from './components/Main';
+import SpeedDials from './components/FABButton';
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
       <Main></Main>
+      
     </div>
   );
 }
--- bunkerweb/src/components/Main.js
+++ bunkerweb/src/components/Main.js
@@ -2,16 +2,22 @@ import React from 'react';
 import './Main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from 'react-bootstrap/Row'
-import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
+import SpeedDials from './FABButton'
 
 export default class Main extends React.Component {
     render() {
         return (
-            <Row xs={2} md={4} lg={6}>
-                <Col className="leftside" md={6}></Col>
-                <Col className="rightside" md={6}></Col>
+            // <Row xs={2} md={4} lg={6}>
+            <Row>
+                {/* <Col className="leftside" md={6}>
+                </Col>
+                <Col className="rightside" md={6}>
+            
+                </Col> */}
+                <SpeedDials></SpeedDials>
             </Row>
         )
     }
 }
+
